5a95597d2d8451ce81c769ed6eeb82c0a6fa2387
edyMartin <evalerio.sdc@hotmail.com>
2023-04-01 23:57:50 -0600

    plantilla de personaje uno agregada

--- src/Levels/LevelOne.js
+++ src/Levels/LevelOne.js
@@ -9,10 +9,10 @@ import TerrainPhysics from "../Pysichs/TerrainPhysics";
 import Load from "../Helpers/Load";
 import JoistycCharacterOne from "../Joistycs/CharacterOne";
 
-
 const LevelOne = ({ THREE, World }) => {
-  const [status, setStatus] = useState(false)
-  const [wind, setWind] = useState(false)
+  const [status, setStatus] = useState(false);
+  const [wind, setWind] = useState(false);
+  const [addSceneCall, setAddSceneCall] = useState(false);
 
   const [scene, setScene] = useState(new THREE.Scene());
   const [geometry, setGeometry] = useState(
@@ -25,31 +25,27 @@ const LevelOne = ({ THREE, World }) => {
     new THREE.WebGLRenderer({ antialias: true })
   );
 
-
-  const [CharacterOnes] = useState(new CharacterOne(scene))
-  const [CharacterOnePysic, setCharacterOnePysic] = useState(undefined)
-  const [Terrains , setTerrains] = useState(undefined)
+  const [CharacterOnes, setCharacterOnes] = useState(undefined);
+  const [CharacterOnePysic, setCharacterOnePysic] = useState(undefined);
+  const [Terrains, setTerrains] = useState(undefined);
   const clock = new THREE.Clock();
-
-
 
   // animate 30 fps
   const animation = () => {
-    let mixer = CharacterOnes.getMixer()
+    let mixer = CharacterOnes.GetMixer();
 
     if (mixer !== undefined) {
-      mixer.update(clock.getDelta())
+      mixer.update(clock.getDelta());
     }
 
-    let object3d = CharacterOnes.Object()
+    let object3d = CharacterOnes.GetObject();
     if (object3d) {
-      object3d.position.copy(CharacterOnePysic.getPosition())
-      object3d.quaternion.copy(CharacterOnePysic.getQuaternion())
-      scene.add(object3d)
+      object3d.position.copy(CharacterOnePysic.getPosition());
+      object3d.quaternion.copy(CharacterOnePysic.getQuaternion());
+      scene.add(object3d);
     }
-
-    World.play()
-    World.step()
+    World.play();
+    World.step();
     renderer.render(scene, UniversalCamera);
   };
 
@@ -61,7 +57,7 @@ const LevelOne = ({ THREE, World }) => {
     placeHold.appendChild(renderer.domElement);
     renderer.setSize(window.innerWidth, window.innerHeight);
     // llamamos a las funciones necesarias
-    AddOnWord()
+    AddOnWord();
 
     const color = 0xffffff;
     const intensity = 2;
@@ -69,69 +65,65 @@ const LevelOne = ({ THREE, World }) => {
     light.position.set(-1, 2, 4);
     scene.add(light);
 
-    CharacterOnes.load()
+    // CharacterOnes.load()
 
     renderer.setAnimationLoop(animation);
   };
 
   // add at word
   const AddOnWord = () => {
-    let addPhysicsCharacterOne = World.add(CharacterOnePysics)
-    let TerrainPhysic = World.add(TerrainPhysics)
-    setCharacterOnePysic(addPhysicsCharacterOne)
-    setTerrains(TerrainPhysic)
-
-  }
+    let addPhysicsCharacterOne = World.add(CharacterOnePysics);
+    let TerrainPhysic = World.add(TerrainPhysics);
+    setCharacterOnePysic(addPhysicsCharacterOne);
+    setTerrains(TerrainPhysic);
+  };
 
   const AddScene = () => {
-
-  }
+    setCharacterOnes(new CharacterOne(scene));
+  };
 
   // this is a resource load
   const ResourceLoad = () => {
-    AddOnWord()
-    var statused = false
-    console.log('ready charge resource')
+    AddOnWord();
+    var statused = false;
+    if (!addSceneCall) {
+      AddScene();
+      setAddSceneCall(true);
+    }
     if (
+      CharacterOnes !== undefined &&
       CharacterOnePysic !== undefined &&
       Terrains !== undefined &&
       scene
     ) {
-      AddScene()
-      JoistycCharacterOne(CharacterOnePysic)
-      statused = true
+      JoistycCharacterOne(CharacterOnePysic);
+      statused = true;
     }
 
-    return statused
-  }
+    return statused;
+  };
 
   useEffect(() => {
     if (!status || !wind) {
-      let ResourceLoadStatus = ResourceLoad()
+      let ResourceLoadStatus = ResourceLoad();
       setStatus(ResourceLoadStatus);
 
       if (document.getElementById("placeHold")) {
-        Init()
-        setWind(true)
+        Init();
+        setWind(true);
       }
-
     }
   });
 
   return (
     <>
       {status && (
-        <div id="placeHold" style={{ width: "100%", height: "100vh" }}>
-
-        </div>
+        <div id="placeHold" style={{ width: "100%", height: "100vh" }}></div>
       )}
 
-      {!status && (
-        <Load></Load>
-      )
-      }
+      {!status && <Load></Load>}
     </>
-  )
+  );
 };
 
 export default LevelOne;
--- src/Objects/CharacterOne/index.js
+++ src/Objects/CharacterOne/index.js
@@ -1,100 +1,164 @@
 import { FBXLoader } from "three/examples/jsm/loaders/FBXLoader";
-import edy from "./CharacterOne.fbx";
+import Walker from "./CharacterOne.fbx";
+import Crying from "./Crying.fbx";
 import * as THREE from "three";
 
 class CharacterOne {
   scene;
 
   constructor(scene) {
+    console.log("LLamamos a character one", Date.now());
     this.scene = scene;
     this.loader = new FBXLoader();
+    this.current = "Walk";
+    /*
     this.mixer = undefined;
     this.mxPlay = undefined;
     this.animation = undefined;
     this.object3d = undefined;
     this.anim = 1;
-    this.pysics = undefined
+    this.pysics = undefined;
+
+    this.animStop = undefined;
+    //cargamos las animaciones completas
+    this.walker();
+    document.addEventListener("keydown", () => {
+      if (this.animStop !== undefined) {
+        this.animate(this.animStop);
+      }
+    });
+    */
+
+    //this is for loader
+    this.Animations = undefined;
+    this.Loader(Walker, "Walk");
+    this.Loader(Crying, "Crying");
+    this.Play();
   }
 
-  load() {
-    let ctx = this;
+  /**
+   * { Actor } 3DObject [FBX]
+   */
+  Loader(Object3D, Name) {
+    let _this = this;
+    let Actor3D = undefined;
+
     this.loader.load(
-      edy,
-      function (object3d) {
-        ctx.object3d = object3d;
-        ctx.object3d.scale.multiplyScalar(0.009);
-        ctx.object3d.position.set(0, 0, 0);
+      Object3D,
 
-        console.log("Hola mundo",object3d);
+      (Actor) => {
+        // console.log("le daremos este nombre ", x)
+        Actor3D = Actor;
+        Actor3D.scale.multiplyScalar(0.009);
+        Actor3D.position.set(0, 0, 0);
 
-        ctx.mixer = new THREE.AnimationMixer(ctx.object3d);
-        ctx.animation = ctx.object3d.animations[ctx.anim];
-
-        ctx.mxPlay = ctx.mixer.clipAction(ctx.animation);
-        ctx.mxPlay.play();
-        console.log(ctx.mxPlay)
-
-        //this.scene.add(object3d);
-      },
-      function (load) {
-        
-      },
-      function (e) {
-        
+        _this.Animations = {
+          ..._this.Animations,
+          [`${Name}`]: {
+            Mixer: new THREE.AnimationMixer(Actor3D),
+            Animations: Actor3D.animations,
+            Object3D: Actor3D,
+          },
+        };
       }
     );
+
+    return Actor3D;
   }
 
-  getMixer() {
-    return this.mixer;
+  Play() {
+    let Checker = setInterval(() => {
+      if (this.Animations !== undefined) {
+        let x = this.scene.add(this.Animations[this.current].Object3D);
+        if (x) {
+          clearInterval(Checker);
+          this.Animation();
+        }
+      } else {
+        console.log("aun no se carga esperando");
+      }
+    }, 100);
   }
 
-  Object() {
-    return this.object3d;
-  }
-
-  mPlay(){
-    return this.mxPlay
-  }
-}
-
-/*
-const CharacterOne = async(scene, THREE) => {
-  let loader = new FBXLoader();
-  var mixer ;
-
-  await loader.load(
-    edy,
-    function (object3d) {
-      console.log("loded", object3d);
-      object3d.scale.multiplyScalar(0.01);
-      object3d.position.set(1, 2, 0);
-
-      mixer = new THREE.AnimationMixer(object3d);
-      var animation = object3d.animations[1];
-
-      var mxPlay = mixer.clipAction(animation);
-      mxPlay.play();
-      
-      //
-
-      object3d.traverse((child) => {
-        child.castShadow = true;
-      });
-      scene.add(object3d);
-    },
-    function (load) {
-      console.log("OnLoadObject", load);
-    },
-    function (e) {
-      console.log("OnErrorLoad", e);
+  GetMixer() {
+    if (this.Animations !== undefined) {
+      return this.Animations[this.current].Mixer;
     }
-  );
-
-  return {
-    mixer
   }
-};
-*/
+
+  GetObject() {
+    if (this.Animations !== undefined) {
+      return this.Animations[this.current].Object3D;
+    }
+  }
+
+  Animation() {
+    if (this.Animations !== undefined) {
+      let currentPerson = this.Animations[this.current];
+      let currentMixer = currentPerson.Mixer;
+      let animation = currentPerson.Animations[1];
+      let clipAnimation = currentMixer.clipAction(animation);
+      clipAnimation.play();
+    }
+  }
+
+  // load() {
+  //   let ctx = this;
+  //   this.loader.load(
+  //     stopping,
+  //     function (object3d) {
+  //       ctx.object3d = object3d;
+  //       ctx.object3d.scale.multiplyScalar(0.009);
+  //       ctx.object3d.position.set(0, 0, 0);
+
+  //       console.log("Hola mundo objecto 3d -->", object3d);
+
+  //       ctx.mixer = new THREE.AnimationMixer(ctx.object3d);
+  //       ctx.animation = ctx.object3d.animations[ctx.anim];
+
+  //       ctx.mxPlay = ctx.mixer.clipAction(ctx.animation);
+  //       ctx.mxPlay.play();
+  //       console.log(ctx.mxPlay);
+
+  //       //this.scene.add(object3d);
+  //     },
+  //     function (load) {},
+  //     function (e) {}
+  //   );
+  // }
+
+  // walker() {
+  //   let ctx = this;
+  //   this.loader.load(
+  //     walker,
+  //     function (object3d) {
+  //       ctx.animStop = object3d;
+  //       ctx.animStop.scale.multiplyScalar(0.009);
+  //     },
+  //     function (load) {},
+  //     function (e) {}
+  //   );
+  // }
+
+  // animate(animation3D) {
+  //   this.mixer = new THREE.AnimationMixer(animation3D);
+  //   this.animation = animation3D.animations[1];
+
+  //   this.mxPlay = this.mixer.clipAction(this.animation);
+  //   this.mxPlay.play();
+  // }
+
+  // getMixer() {
+  //   return this.mixer;
+  // }
+
+  // Object() {
+  //   return this.object3d;
+  // }
+
+  // mPlay() {
+  //   return this.mxPlay;
+  // }
+}
 
 export default CharacterOne;
