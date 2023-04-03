6269ecb902eea94ea24b2906c57893b0d419f637
edyMartin <edgarmartinx@icloud.com>
2023-04-03 15:35:27 -0600

    agregamos varias animaciones

--- src/Objects/CharacterOne/index.js
+++ src/Objects/CharacterOne/index.js
@@ -1,8 +1,9 @@
 import { FBXLoader } from "three/examples/jsm/loaders/FBXLoader";
 import Crying from "./Crying.fbx";
 
-import Walk from "../../Actors/Scarlett/Standing.fbx"
+import Walk from "../../Actors/Scarlett/Walking.fbx"
 import scarlettStanding from "../../Actors/Scarlett/Standing.fbx";
+import Jump from "../../Actors/Scarlett/Jumping.fbx";
 import * as THREE from "three";
 
 class CharacterOne {
@@ -17,7 +18,9 @@ class CharacterOne {
 
     this.Loader(scarlettStanding, 'Standing');
     this.Loader(Crying, 'Crying')
-	this.Loader(Walk, 'Walk')
+    this.Loader(Walk, 'Walk')
+    this.Loader(Jump, 'Jumping')
+
     this.Actor3D = undefined;
   }
 
@@ -25,7 +28,11 @@ class CharacterOne {
     const Init = setInterval(() => {
       if (this.Pincels.length !== 0) {
         let InitAnimation = this.Pincels[this.CurrentAnimation];
-        InitAnimation.play();
+        try {
+          InitAnimation.play();
+        } catch (e) {
+          window.location.reload()
+        }
         this.Controls();
         clearInterval(Init);
       }
@@ -47,24 +54,57 @@ class CharacterOne {
         console.log("se añade el primero", Date.now());
         _this.Mixer = new THREE.AnimationMixer(_this.Actor3D);
       }
+      console.log(Name, Actor3D)
       let animations = Actor3D.animations;
+      animations.map((i, k) => {
+        if (i.duration > 0) {
+          _this.Pincels = {
+            ...this.Pincels,
+            [`${Name}`]: _this.Mixer.clipAction(animations[k]),
+          };
+        }
+      })
 
-      _this.Pincels = {
-        ...this.Pincels,
-        [`${Name}`]: _this.Mixer.clipAction(animations[1]),
-      };
     });
   }
 
   Controls() {
     document.addEventListener("keydown", (e) => {
-      console.log(this.Pincels);
-        // let InitAnimation = this.Pincels[this.CurrentAnimation];
-        // InitAnimation.stop();
+      let key = e.key
+      let InitAnimation = this.Pincels[this.CurrentAnimation];
+      switch (key) {
+        case 'ArrowUp':
+          let walk = this.Pincels['Walk'];
+          InitAnimation.stop();
+          walk.play()
+          break
+        case ' ':
 
-        // let next = this.Pincels['Crying'];
-        // next.play();
+          let Jumping = this.Pincels['Jumping'];
+
+          InitAnimation.stop();
+          Jumping.play()
+          break
+      }
     });
+
+    document.addEventListener('keyup', (e) => {
+      let key = e.key
+      let InitAnimation = this.Pincels[this.CurrentAnimation];
+      switch (key) {
+        case 'ArrowUp':
+          let walk = this.Pincels['Walk'];
+          walk.stop()
+          InitAnimation.play();
+          break
+        case ' ':
+          let Jumping = this.Pincels['Jumping'];
+          Jumping.stop()
+          InitAnimation.play();
+          break
+      }
+      console.log(e)
+    })
   }
 
   GetObject() {
